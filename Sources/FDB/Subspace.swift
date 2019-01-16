bc148186a341f7c5a576c89706b616cf09418b1d
Kirill Titov <kirill@kirilltitov.com>
2019-01-16 18:42:50 +0300

    Better threading, Subspace items counter (#13)
    * Improve threading
    
    * Add Subspace items counter

--- Sources/FDB/Subspace.swift
+++ Sources/FDB/Subspace.swift
@@ -1,5 +1,6 @@
 public struct Subspace {
     public let prefix: Bytes
+    public let itemsCount: Int
 
     public var range: RangeFDBKey {
         return (
@@ -8,20 +9,21 @@ public struct Subspace {
         )
     }
 
-    public init(_ prefix: Bytes) {
+    public init(_ prefix: Bytes, items: Int = 1) {
         self.prefix = prefix
+        self.itemsCount = items
     }
 
     public init(_ input: TuplePackable?...) {
-        self.init(Tuple(input))
+        self.init(Tuple(input), items: input.count)
     }
 
-    public init(_ tuple: Tuple) {
-        self.init(tuple.pack())
+    public init(_ tuple: Tuple, items: Int = 1) {
+        self.init(tuple.pack(), items: items)
     }
 
     public func subspace(_ input: [TuplePackable?]) -> Subspace {
-        return Subspace(self.prefix + Tuple(input).pack())
+        return Subspace(self.prefix + Tuple(input).pack(), items: self.itemsCount + input.count)
     }
 
     public func subspace(_ input: TuplePackable?...) -> Subspace {
