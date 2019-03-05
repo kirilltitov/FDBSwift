3bba56edde4b80ea6d01f27ce2eafe4ecc54549e
Kirill Titov <work@kirilltitov.com>
2019-03-05 23:17:56 +0300

    Builtin blocking operations are utilizing `withTransaction`

--- Sources/FDB/FDB.swift
+++ Sources/FDB/FDB.swift
@@ -214,7 +214,20 @@ public class FDB {
     ///   - key: FDB key
     ///   - value: bytes value
     public func set(key: AnyFDBKey, value: Bytes) throws {
-        try self.begin().set(key: key, value: value, commit: true) as Void
+        try self.withTransaction {
+            try $0.set(key: key, value: value, commit: true) as Void
+        }
+    }
+
+    /// Sets bytes to given key in FDB cluster
+    ///
+    /// This function will block current thread during execution
+    ///
+    /// - parameters:
+    ///   - key: FDB key
+    ///   - value: bytes value
+    public func setSync(key: AnyFDBKey, value: Bytes) throws {
+        try self.set(key: key, value: value) as Void
     }
 
     /// Clears given key in FDB cluster
@@ -224,7 +237,19 @@ public class FDB {
     /// - parameters:
     ///   - key: FDB key
     public func clear(key: AnyFDBKey) throws {
-        return try self.begin().clear(key: key, commit: true)
+        try self.withTransaction {
+            try $0.clear(key: key, commit: true) as Void
+        }
+    }
+
+    /// Clears given key in FDB cluster
+    ///
+    /// This function will block current thread during execution
+    ///
+    /// - parameters:
+    ///   - key: FDB key
+    public func clearSync(key: AnyFDBKey) throws {
+        try self.clear(key: key) as Void
     }
 
     /// Clears keys in given range in FDB cluster
@@ -235,7 +260,20 @@ public class FDB {
     ///   - begin: Begin key
     ///   - end: End key
     public func clear(begin: AnyFDBKey, end: AnyFDBKey) throws {
-        return try self.begin().clear(begin: begin, end: end, commit: true)
+        try self.withTransaction {
+            try $0.clear(begin: begin, end: end, commit: true) as Void
+        }
+    }
+
+    /// Clears keys in given range in FDB cluster
+    ///
+    /// This function will block current thread during execution
+    ///
+    /// - parameters:
+    ///   - begin: Begin key
+    ///   - end: End key
+    public func clearSync(begin: AnyFDBKey, end: AnyFDBKey) throws {
+        try self.clear(begin: begin, end: end) as Void
     }
 
     /// Clears keys in given range in FDB cluster
@@ -266,7 +304,9 @@ public class FDB {
     ///   - key: FDB key
     ///   - snapshot: Snapshot read (i.e. whether this read create a conflict range or not)
     public func get(key: AnyFDBKey, snapshot: Bool = false) throws -> Bytes? {
-        return try self.begin().get(key: key, snapshot: snapshot, commit: true)
+        return try self.withTransaction {
+            try $0.get(key: key, snapshot: snapshot, commit: true)
+        }
     }
 
     /// Returns a range of keys and their respective values under given subspace
@@ -277,7 +317,9 @@ public class FDB {
     ///   - subspace: Subspace
     ///   - snapshot: Snapshot read (i.e. whether this read create a conflict range or not)
     public func get(subspace: Subspace, snapshot: Bool = false) throws -> KeyValuesResult {
-        return try self.begin().get(range: subspace.range, snapshot: snapshot)
+        return try self.withTransaction {
+            try $0.get(range: subspace.range, snapshot: snapshot)
+        }
     }
 
     /// Returns a range of keys and their respective values in given key range
@@ -311,21 +353,23 @@ public class FDB {
         snapshot: Bool = false,
         reverse: Bool = false
     ) throws -> FDB.KeyValuesResult {
-        return try self.begin().get(
-            begin: begin,
-            end: end,
-            beginEqual: beginEqual,
-            beginOffset: beginOffset,
-            endEqual: endEqual,
-            endOffset: endOffset,
-            limit: limit,
-            targetBytes: targetBytes,
-            mode: mode,
-            iteration: iteration,
-            snapshot: snapshot,
-            reverse: reverse,
-            commit: true
-        )
+        return try self.withTransaction {
+            try $0.get(
+                begin: begin,
+                end: end,
+                beginEqual: beginEqual,
+                beginOffset: beginOffset,
+                endEqual: endEqual,
+                endOffset: endOffset,
+                limit: limit,
+                targetBytes: targetBytes,
+                mode: mode,
+                iteration: iteration,
+                snapshot: snapshot,
+                reverse: reverse,
+                commit: true
+            )
+        }
     }
 
     /// Returns a range of keys and their respective values in given key range
@@ -382,7 +426,9 @@ public class FDB {
     ///   - key: FDB key
     ///   - value: Value bytes
     public func atomic(_ op: FDB.MutationType, key: AnyFDBKey, value: Bytes) throws {
-        try self.begin().atomic(op, key: key, value: value, commit: true) as Void
+        try self.withTransaction {
+            try $0.atomic(op, key: key, value: value, commit: true) as Void
+        }
     }
 
     /// Peforms an atomic operation in FDB cluster on given key with given signed integer value
@@ -410,13 +456,17 @@ public class FDB {
     ///   - key: FDB key
     ///   - value: Integer
     @discardableResult public func increment(key: AnyFDBKey, value: Int64 = 1) throws -> Int64 {
-        let transaction = try self.begin()
-        try transaction.atomic(.add, key: key, value: getBytes(value), commit: false) as Void
-        guard let bytes: Bytes = try transaction.get(key: key) else {
-            throw FDB.Error.unexpectedError("Couldn't get key '\(key)' after increment")
+        return try self.withTransaction { transaction in
+            try transaction.atomic(.add, key: key, value: getBytes(value), commit: false) as Void
+
+            guard let bytes: Bytes = try transaction.get(key: key) else {
+                throw FDB.Error.unexpectedError("Couldn't get key '\(key)' after increment")
+            }
+
+            try transaction.commitSync()
+
+            return bytes.cast()
         }
-        try transaction.commitSync()
-        return bytes.cast()
     }
 
     /// Peforms a quasi-atomic decrement operation in FDB cluster on given key with given integer
