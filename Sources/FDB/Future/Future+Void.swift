a95e2681dd7e023529d7ce4dab4d489e2bc99510
Kirill Titov <kirill@kirilltitov.com>
2019-03-29 11:32:55 +0300

    Migrate to SPM 5.0 (#39)

--- Sources/FDB/Future/Future+Void.swift
+++ Sources/FDB/Future/Future+Void.swift
@@ -2,7 +2,7 @@ import CFDB
 
 extension FDB.Future {
     /// Sets a closure to be executed when current future is resolved
-    func whenVoidReady(_ callback: @escaping () -> Void) {
-        self.whenReady { _ in callback() }
+    func whenVoidReady(_ callback: @escaping (()) -> Void) {
+        self.whenReady { _ in callback(()) }
     }
 }
