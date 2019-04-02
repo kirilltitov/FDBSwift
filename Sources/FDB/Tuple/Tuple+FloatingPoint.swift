d66ade5af8f1cd63406b74833fcaef018d5b4362
Kirill Titov <kirill@kirilltitov.com>
2019-04-03 01:09:56 +0300

    Update Tuple+FloatingPoint.swift

--- Sources/FDB/Tuple/Tuple+FloatingPoint.swift
+++ Sources/FDB/Tuple/Tuple+FloatingPoint.swift
@@ -1,9 +1,9 @@
 fileprivate func transformFloatingPoint(bytes: inout Bytes, start: Int, encode: Bool) {
-    if (encode && (bytes[start] & 0x80) != 0x00) {
+    if encode && (bytes[start] & 0x80) != 0x00 {
         for i in start ..< bytes.count {
             bytes[i] = bytes[i] ^ 0xff
         }
-    } else if (!encode && (bytes[start] & 0x80) != 0x80) {
+    } else if !encode && (bytes[start] & 0x80) != 0x80 {
         for i in start ..< bytes.count {
             bytes[i] = bytes[i] ^ 0xff
         }
