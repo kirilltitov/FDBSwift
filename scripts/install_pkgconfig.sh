fd98c6e5eb7af1e2f3310ab66ec694993b751870
Kirill Titov <kirill@kirilltitov.com>
2021-12-09 01:21:46 +0300

    Fix DispatchSemaphore wait helper (#81)
    * Fix DispatchSemaphore wait helper
    
    For some reason it broke. Make it working again.
    
    * Upgrade script

--- scripts/install_pkgconfig.sh
+++ scripts/install_pkgconfig.sh
@@ -1,12 +1,31 @@
 #!/usr/bin/env bash
 
+if [ $EUID != 0 ]; then
+    if [ "$(uname)" == "Darwin" ]; then
+        echo "This script requires privileged access in order to put `
+            `a pkgconfig file into /usr/local/lib/pkgconfig `
+            `(and also create it if it doesn't exist yet, it happens on fresh macOS)"
+    elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+        echo "This script requires privileged access in order to put `
+            `a pkgconfig file into /usr/lib/pkgconfig"
+    fi
+
+    sudo "$0" "$@"
+    exit $?
+fi
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 FILE=libfdb.pc
 PKGCONFIG="${DIR}/${FILE}"
 DEST_DIR="lib/pkgconfig/${FILE}"
 
 if [ "$(uname)" == "Darwin" ]; then
-    cp "${PKGCONFIG}.mac" "/usr/local/${DEST_DIR}"
+    _PATH="/usr/local"
+    TARGET="${_PATH}/${DEST_DIR}"
+    mkdir -p $TARGET
+    cp "${PKGCONFIG}.mac" $TARGET
+    LIBPATH="${_PATH}/lib/libfdb_c.dylib"
+    install_name_tool -id $LIBPATH $LIBPATH
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     cp "${PKGCONFIG}.linux" "/usr/${DEST_DIR}"
 fi
